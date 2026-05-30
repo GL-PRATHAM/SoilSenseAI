581f37fa8a9a2f51037f4bf84d1188a9e8f8f718
GL-PRATHAM <titaniumpratham@gmail.com>
2026-05-30 08:56:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(__dirname));
+
 app.get("/", (req, res) => {
-  res.send("SoilSenseAI Backend Running");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
